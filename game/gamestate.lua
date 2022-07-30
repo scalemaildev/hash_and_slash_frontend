b4b49cf0eab2143aaeede2d1fc3240c6b59b7218
scalemaildev <scale@scalemail.com>
2022-07-29 22:18:31 -0500

    update for more html5 tests

--- game/gamestate.lua
+++ game/gamestate.lua
@@ -1,9 +1,14 @@
 Gamestate = {
+	room_id = "",
 	tiles = {},
 	actors = {}
 }
 
 -- SETTERS
+function Gamestate.set_room_id(room_id)
+	Gamestate.room_id = room_id
+end
+	
 function Gamestate.set_tile(pos, tile_type)
 	Gamestate.tiles[pos] = tile_type
 end
@@ -16,6 +21,10 @@ function Gamestate.set_actor(id, pos, actor_type)
 end
 
 -- GETTERS
+function Gamestate.get_room_id()
+	return Gamestate.room_id
+end
+
 function Gamestate.get_tiles()
 	return Gamestate.tiles
 end
